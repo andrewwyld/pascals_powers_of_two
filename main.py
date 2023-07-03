229185bd1545921183a57de485fcc8be764abaa4
Andrew Wyld <awyld@neurofenix.com>
2023-07-03 15:07:42 +0100

    Somewhat more useful
    Intractable for large sums, though

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-MAX_DEPTH = 1024
+MAX_DEPTH = 32
 
 
 def pascal_value_for(row, i):
@@ -6,22 +6,29 @@ def pascal_value_for(row, i):
         return row[0]
     if i == len(row):
         return row[-1]
-    return row[i-1] + row[i]
+    return row[i - 1] + row[i]
 
 
 def pascal(row):
     return [pascal_value_for(row, i) for i in range(len(row) + 1)]
 
 
+def print_sum(row, i):
+    output = f"{row[0]}"
+    for j in range(1, i):
+        output += f" + {row[j]}"
+    return output
+
+
 def find_powers_of_two(row):
     power_found = False
     powers = []
     total = row[0]  # ignore 1 = 2⁰
-    for i in range(1, len(row)):
+    for i in range(1, len(row) - 1):  # also ignore entire row sum
         total += row[i]
         if bin(total).count("1") == 1:
             power_found = True
-            powers.append((i, total))
+            powers.append((i + 1, (print_sum(row, i + 1))))  # correct for zero indexing
     return power_found, powers
 
 
@@ -39,4 +46,3 @@ if __name__ == '__main__':
 
     for power in powers_list:
         print(f"{power[0]}\t{power[1][0]}\t{power[1][1]}")
-
